a8d26dce9b501e389c59604f9bf25d929b03f750
wuzihao9999 <44420520+wuzihao9999@users.noreply.github.com>
2018-11-29 22:20:27 -0500

    Update code
    Add the beginning code for the project, which is used to get some input from the user.

--- Project_Blacklitterman_Model.py
+++ Project_Blacklitterman_Model.py
@@ -9,6 +9,55 @@ Created on Thu Nov 18 10:46:54 2018
 import scipy.optimize
 from numpy import *
 from pandas import *
+import re
+
+
+
+##At the beginning of the project, we need to ask users for some specific variables, including: #########
+##The choices of assets for portfolio, which are chosen from the sample set provided by project #########
+##The view given by the investor#########################################################################
+print('Welcome to use the Blacklitterman Model project.')
+print('Based on the performance, we provided 20 assets that are worthy investing.')
+print('The symbols of these 20 assets are as follows:')
+print('1 GE; 2 CVX; ...')
+print('Please select assets that you want to invest: ')
+print('(enter the number of the assets, e.g. if you want to invest GE, type in 1; CVX for 2;...)')
+illegal_asset = 100
+judgement = 0
+print('(please use list to type in, e.g. [1,4,6,7,9])')
+while illegal_asset > 0 or judgement < 1:
+    number_of_assets = input('hhaha')
+    pattern1 = r'[0-9.]+'
+    type_in_assets = re.findall(pattern1,number_of_assets)
+    illegal_asset = 0
+    select_assets = []
+    for i in type_in_assets:
+        a = int(float(i))
+        if a < 0 or a > 20 or a != float(i):
+            illegal_asset += 1
+            print('Please type in integer numbers between 1-20!')
+        exist = 0
+        for j in select_assets:
+            if j == a:
+                exist += 1
+        if exist >= 1:
+            continue
+        else:
+            select_assets.append(a)
+    if illegal_asset == 0:
+        print(f'Your choices are assets: {select_assets}')
+        print('Please verify your choice: 1 for Yes, 0 for No.')
+        judge = input('judge = ')
+        if int(float(judge)) == 1:
+            judgement == 1
+            break
+        else:
+            judgement == 0
+            print('Please choose again:')
+print(f'Your choices are assets: {select_assets}')
+print('Now, please enter your view toward assets using specific style.')
+#####To be continue##
+
 
 ##The function to calculate the valid frontier of portfolio constructed with given assets##
 def frontier_of_portfolio(Rp,Vp,rf):

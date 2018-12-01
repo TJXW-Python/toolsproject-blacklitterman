1352135574fa2795dd4f9eb52c16a2a1cb48ef19
James-Jia <43897573+James-Jia@users.noreply.github.com>
2018-12-01 00:43:16 -0500

    Update Calculate weights, return and var_cov matrix from data.py

--- Calculate weights, return and var_cov matrix from data.py
+++ Calculate weights, return and var_cov matrix from data.py
@@ -1,5 +1,4 @@
 import numpy 
-
 #Functions get market capitilizaions from our chosen data
 #and use these data to calculate weights, return and covariance
 
@@ -7,22 +6,23 @@ def compute_some_statistics(price_arrays):
     price = numpy.array(price_arrays)
     
     price_row, price_col = price.shape
-    data_return = numpy.zeros((price_row, price_col - 1))
-    for r in range(price_row):
-        for c in range(price_col - 1):
-            p_start = price[r, c]
-            p_end = price[r, c + 1]
-            data_return[r, c] = (p_end - p_start)/ p_start
+    data_return = numpy.zeros([price_row, price_col - 1])
+    for i in range(price_row):
+        for j in range(price_col - 1):
+            p_start = price[i, j]
+            p_end = price[i, j + 1]
+            data_return[i, j] = (p_end - p_start)/ p_start
             
     cal_return = numpy.array([])
-    for r in range(price_row):
-        cal_return = append(cal_return, numpy.mean(data_return[r]))
+    for i in range(price_row):
+        cal_return = append(cal_return, numpy.mean(data_return[i]))
     var_cov_matx = cov(data_return)
+    #We assume that there are 250 tarding days in one year, and calculate annulized return
+    #and annulized var_cov matrix
     cal_return = (1 + cal_return) ** 250 - 1
     var_cov_matx  = var_cov_matx * 250
     return cal_return, var_cov_matx
 
 W = np.array(caps) / sum(caps)
-Rp = compute_some_statistics(price_arrays)
-Vp = compute_some_statistics(price_arrays)
+Rp, Vp= compute_some_statistics(price_arrays)
 rf = 0.015

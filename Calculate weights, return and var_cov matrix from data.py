c420730358aeade91fd0665a5fae2e06fe27e429
James-Jia <43897573+James-Jia@users.noreply.github.com>
2018-11-30 21:37:42 -0500

    Update Calculate weights, return and var_cov matrix from data.py

--- Calculate weights, return and var_cov matrix from data.py
+++ Calculate weights, return and var_cov matrix from data.py
@@ -1,24 +1,24 @@
-import numpy as np
+import numpy 
 
 #Functions get market capitilizaions from our chosen data
 #and use these data to calculate weights, return and covariance
 
 def compute_some_statistics(price_arrays):
-    price = np.array(price_arrays)
+    price = numpy.array(price_arrays)
     
-    price_row, price_col = price_arrays.shape
-    data_return = np.zeros((price_row, price_col - 1))
+    price_row, price_col = price.shape
+    data_return = numpy.zeros((price_row, price_col - 1))
     for r in range(price_row):
         for c in range(price_col - 1):
-            p_start = price_arrays[r, c]
-            p_end = price_arrays[r, c + 1]
+            p_start = price[r, c]
+            p_end = price[r, c + 1]
             data_return[r, c] = (p_end - p_start)/ p_start
             
-    cal_return = np.array([])
+    cal_return = numpy.array([])
     for r in range(price_row):
-        cal_return = append(expreturns, numpy.mean(returns[r]))
+        cal_return = append(cal_return, numpy.mean(data_return[r]))
     var_cov_matx = cov(data_return)
-    cal_return = (1 + expreturns) ** 250 - 1
+    cal_return = (1 + cal_return) ** 250 - 1
     var_cov_matx  = var_cov_matx * 250
     return cal_return, var_cov_matx
 

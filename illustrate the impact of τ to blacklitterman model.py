4913803b084dbb26a708525ba4b730513d81d5d0
James-Jia <43897573+James-Jia@users.noreply.github.com>
2018-12-01 11:41:41 -0500

    Update illustrate the impact of τ to blacklitterman model.py

--- illustrate the impact of τ to blacklitterman model.py
+++ illustrate the impact of τ to blacklitterman model.py
@@ -7,14 +7,17 @@
 # τ too high makes a very weak statement for our prior estimate of the mean.
 def optimization_adding_views(Vp,view,view_link,Pi,rf):
 
-    def pi_add_view(Vp,view,view_link,Pi):  
-        scalar = numpy.linspace(0.025, 0.05, 6)      # scaling factor for blacklitterman model is set according to Lee's paper
+    def pi_add_view(Vp,view,view_link,Pi):
+        # scaling factor for blacklitterman model is set bewtween a range
+        scalar = numpy.linspace(0.025, 0.05, 6)      
         ##weight is measured by uncertainty, this is the uncertainty matrix of equilibrium excess return: pi_vol (n*n matrix)
         scalar_list = list(scalar)
+        len_list = len(scalar_list)
+        # Obtain a list of pi_new values
         pi_new_list = []
-        for i in scalar_list:
-            pi_vol = dot(i,Vp)                    
-            view_vol= dot(dot(dot(i,view_link),Vp),transpose(view_link))  
+        for i in range(len_list):
+            pi_vol = dot(scalar_list[i],Vp)                    
+            view_vol= dot(dot(dot(scalar_list[i],view_link),Vp),transpose(view_link))  
             pi_vol_inv = inv(pi_vol)                
             view_vol_inv = dot(dot(transpose(view_link),inv(view_vol)),view_link) 
             pi_view_weighted =dot(pi_vol_inv,Pi)+dot(dot(transpose(view_link),inv(view_vol)),view) 
@@ -25,7 +28,6 @@ def optimization_adding_views(Vp,view,view_link,Pi,rf):
         return pi_new_list                         
 
     # get the optimal allocation weights and efficent frontier based on new equilibrium excess return
-    len_list = len(pi_new_list)
     pi_new_list = pi_add_view(Vp,view,view_link,Pi)  
     Wp_new_list =[weight_MV(i + rf,Vp,rf) for i in pi_new_list] 
     mean_new_list = [sum(Wp_new_list[i] * (pi_new_list[i] + rf)) for i in range(len_list)]

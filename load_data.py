2353749fda7d5cc9776f021016817138839e971d
sockiesss <44385315+sockiesss@users.noreply.github.com>
2018-11-30 19:13:45 -0500

    Update load_data.py
    Modify the load_data function so that it can take in a list of assets and extract market caps corresponding to the stock symbols entered by the user and also loads the historical stock prices

--- load_data.py
+++ load_data.py
@@ -1,12 +1,20 @@
 from pandas import *
 
+base = 'data/'
+pool_file = base + 'SymbolsAndCaps.csv'
 
-# Function loads historical stock prices of S&P companies mentioned in a CSV file and returns them together
-# with their market capitalizations, as of Nov 29, 2018
-def load_data(file):
-    symbols, caps = load_symbols_and_caps(file)
-    n = len(symbols)
-    price_arrays = [] # array of close prices of each stock
+# Function extract market caps corresponding to the stock symbols entered by the user
+# and also loads the historical stock prices and returns stock symbols, caps and historical prices
+def load_data(assets_list):
+    
+    pool_symbols, pool_caps = load_symbols_and_caps(pool_file) # first load all symbols and caps in the pool
+    
+    symbols = assets_list
+    caps = [] # extract market caps of the stocks chosen by the user
+    for s in symbols:
+        caps.append(pool_caps[pool_symbols.index(s)])
+
+    price_arrays = [] # array of close prices of each stock chosen by the user
     for s in symbols:
         data = pandas.read_csv('data/%s.csv' % s, index_col=None, parse_dates=['date']) # data is a pandas.dataframe
         prices = list(data['close'])

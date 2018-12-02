79f388573b759985f8ec294f83bf6eae1072f7df
fionaxjh <44420616+fionaxjh@users.noreply.github.com>
2018-12-02 17:25:29 -0500

    Adding output collections into the body

--- Project_Blacklitterman_Model.py
+++ Project_Blacklitterman_Model.py
@@ -340,19 +340,32 @@ def graph_names(names, Rp, Vp, color='black'):
     for i in range(len(Rp)): 
         text(Vp[i, i] ** .5, Rp[i], '  %s' % names[i], verticalalignment='center', color=color) 
 
+##################################################################################################
+
 #Collect the optimal weight of 3 models
 optimal_historical_data = optimal_portfolio_based_on_equilibrium_returns(Rp,Vp,rf)
 optimal_implied_excess_return =optimal_portfolio_based_on_equilibrium_returns(Pi+rf,Vp,rf)
 optimal_adding_views = optimization_adding_views(Vp,view,view_link,Pi,rf)
 
+##output of the whole model reslut into dataframe and graphs:
+def OUTPUT_MODEL(optimal_historical_data,optimal_implied_excess_return,optimal_adding_views):
+    #Graph based on comparison among three models
+    graph_names(names,Rp,Vp,color='blue')
+    graph_efficient_frontier(optimal_historical_data,label='Historical Data', color='blue')
 
-graph_names(names,Rp,Vp,color='blue')
-graph_efficient_frontier(optimal_historical_data,label='Historical Data', color='blue')
+    graph_names(names, Pi+rf, Vp, color='green')
+    graph_efficient_frontier(optimal_implied_excess_return,label='Implied Equilibrium Excess Return', color='green')
 
-graph_names(names, Pi+rf, Vp, color='green')
-graph_efficient_frontier(optimal_implied_excess_return,label='Implied Equilibrium Excess Return', color='green')
+    graph_names(names,optimal_adding_views['Pi']+rf, Vp, color='red')
+    graph_efficient_frontier(optimal_adding_views,label='Adding views', color='red')
 
-graph_names(names,optimal_adding_views['Pi']+rf, Vp, color='red')
-graph_efficient_frontier(optimal_adding_views,label='Adding views', color='red')
+    xlabel('variance $\sigma$'), ylabel('mean $\mu$'), legend(), show()
+    
+    #optimal weights based on three given models:
+    display(pandas.DataFrame({'Return': Rp, 'Weight (original_MV_opt)': weight_MV(Rp,Vp,rf),
+                          'Weight (Reverse Optimization)': optimal_portfolio_based_on_equilibrium_returns(Pi+rf,Vp,rf)['Weights'],
+                          'Weight (reverse opt)': optimal_adding_views['Weights']}, index=names).T)
+    display(pandas.DataFrame(Vp, columns=names, index=names))
 
-xlabel('variance $\sigma$'), ylabel('mean $\mu$'), legend(), show()
+    
+    ###########################################################

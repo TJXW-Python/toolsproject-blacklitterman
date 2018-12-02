e3a165f1211aee476b8d5214742b8cd2d61ac052
wuzihao9999 <44420520+wuzihao9999@users.noreply.github.com>
2018-12-02 00:38:13 -0500

    Update code
    Update part of the code, which is used for interaction

--- interact with user to ask for views_relative.py
+++ interact with user to ask for views_relative.py
@@ -14,48 +14,53 @@ relat_view_judge = 1
 while relat_view_judge:
     relat_view_judge = 0
     relat_view_ori = input()
-    relat_pattern = r'[A-Za-z-]+\W*[<>]\W*[A-Za-z-]+\W*[0-9.-]+'
-    user_relat_view_str = re.findall(relat_pattern,relat_view_ori)
-    if not user_relat_view_str:
-            print('Please input valid relative views!\nPlease input again:')
-            relat_view_judge = 1
-            continue
-    relat_view_list = []
-    for i in user_relat_view_str:
-        relat_both_name = r'^[A-Za-z-]+\W*[<>]\W*[A-Za-z-]+'
-        relat_excess_return = r'[0-9.-]+$'
-        temp_both_name = re.findall(relat_both_name,i)
-        temp_ret = re.findall(relat_excess_return,i)
-        smaller = 0
-        for k in i:
-            if k == '<':
-                smaller = 1
-        relat_name1 = r'^[A-Za-z-]+'
-        relat_name2 = r'[A-Za-z-]+$'
-        temp_name1 = re.findall(relat_name1,temp_both_name[0])
-        temp_name2 = re.findall(relat_name2,temp_both_name[0])
-        name1 = temp_name1[0]
-        name2 = temp_name2[0]
-        if name1 == name2:
-            print('Cannot compare two assets that are the same.')
-            relat_view_judge = 1
-            continue
-        if smaller == 1:
-            temp = name1
-            name1 = name2
-            name2 = temp
-        name_judge = 0
-        for j in symbols:
-            if j == name1 or j == name2:
-                name_judge += 1
-        if name_judge == 2 and float(temp_ret[0]) > 0:
-            relat_view_list.append([name1,name2,temp_ret[0]])#adjust the sequence to make name1 > name2
-        elif name_judge < 2:
-            print('Please input view about assets that you want to invest!\nPlease input your view again!')
-            relat_view_judge = 1
-            continue
-        elif float(temp_ret[0]) <= 0:
-            print('Attention please: the excess return should be positive!\nPlease input your view again!')
-            relat_view_judge = 1
-            continue
-    view['relative'] = relat_view_list
+    if not relat_view_ori:
+        relat_view_judge = 0
+        view['relative'] = []
+        break
+    else:
+        relat_pattern = r'[A-Za-z-]+\W*[<>]\W*[A-Za-z-]+\W*[0-9.-]+'
+        user_relat_view_str = re.findall(relat_pattern,relat_view_ori)
+        if not user_relat_view_str:
+                print('Please input valid relative views!\nPlease input again:')
+                relat_view_judge = 1
+                continue
+        relat_view_list = []
+        for i in user_relat_view_str:
+            relat_both_name = r'^[A-Za-z-]+\W*[<>]\W*[A-Za-z-]+'
+            relat_excess_return = r'[0-9.-]+$'
+            temp_both_name = re.findall(relat_both_name,i)
+            temp_ret = re.findall(relat_excess_return,i)
+            smaller = 0
+            for k in i:
+                if k == '<':
+                    smaller = 1
+            relat_name1 = r'^[A-Za-z-]+'
+            relat_name2 = r'[A-Za-z-]+$'
+            temp_name1 = re.findall(relat_name1,temp_both_name[0])
+            temp_name2 = re.findall(relat_name2,temp_both_name[0])
+            name1 = temp_name1[0]
+            name2 = temp_name2[0]
+            if name1 == name2:
+                print('Cannot compare two assets that are the same.')
+                relat_view_judge = 1
+                continue
+            if smaller == 1:
+                temp = name1
+                name1 = name2
+                name2 = temp
+            name_judge = 0
+            for j in symbols:
+                if j == name1 or j == name2:
+                    name_judge += 1
+            if name_judge == 2 and float(temp_ret[0]) > 0:
+                relat_view_list.append([name1,name2,temp_ret[0]])#adjust the sequence to make name1 > name2
+            elif name_judge < 2:
+                print('Please input view about assets that you want to invest!\nPlease input your view again!')
+                relat_view_judge = 1
+                continue
+            elif float(temp_ret[0]) <= 0:
+                print('Attention please: the excess return should be positive!\nPlease input your view again!')
+                relat_view_judge = 1
+                continue
+        view['relative'] = relat_view_list
